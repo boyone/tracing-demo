38804b670c7db8a2c442d68276adb4ddb70138fa
boyone <boyone.g@gmail.com>
2021-09-12 12:40:31 +0700

    Introduce jaegertracing lib

--- src/main/java/th/in/dminer/tracingdemo/TracingDemoApplication.java
+++ src/main/java/th/in/dminer/tracingdemo/TracingDemoApplication.java
@@ -1,5 +1,8 @@
 package th.in.dminer.tracingdemo;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.JaegerTracer;
+import io.opentracing.Span;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TracingDemoApplication {
 
+	private static JaegerTracer tracer;
+
 	@RequestMapping("/checkout")
 	public String checkout() {
-
+		Span span = tracer.buildSpan("checkout").start();
+		span.finish();
 		return "You have successfully checked out your shopping cart.";
 	}
 
 	public static void main(String[] args) {
+		tracer = Configuration.fromEnv("EShop").getTracer();
 		SpringApplication.run(TracingDemoApplication.class, args);
 	}
 
